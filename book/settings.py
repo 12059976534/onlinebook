33e7e835d3cdfbefa2a8ac309b8372228a59d684
12059976534 <55779796+12059976534@users.noreply.github.com>
2021-02-17 02:42:12 -0500

    Update settings.py
    email seting.py

--- book/settings.py
+++ book/settings.py
@@ -39,7 +39,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jekomontainugrah@gmail.com'
-EMAIL_HOST_PASSWORD = '120599$3'
+EMAIL_HOST_PASSWORD = 'sandi email anda'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Online book<jekomontainugrah@gmail.com>'
 
